baca1182fc27a16aba4c4092c9e72e3bb56c0c23
Jeremy Green <jem.green@outlook.com>
2020-10-28 17:59:28 +0000

    Add project files.

---
+++ LoggerLibrary/LogMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggerLibrary
+{
+    public struct LogMessage
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public string Message { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(LogMessage left, LogMessage right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(LogMessage left, LogMessage right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ LoggerLibrary/PeriodicityOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggerLibrary
+{
+    public enum PeriodicityOptions
+    {
+        Daily,
+        Hourly,
+        Minutely,
+        Monthly
+    }
+}
